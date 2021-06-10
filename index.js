e2e70fa625e2c6dba11b3eada08471bdd98517f4
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-10 13:33:21 -0500

    Functioning routes for each marker that go to route.ejs. Data input required.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const methodOverride = require("method-override");
 const db = require('./models');
 
 const app = express();
+const log = console.log
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
@@ -13,15 +14,6 @@ app.use(express.static(__dirname + '/public/'))
 app.use(methodOverride("_method"))
 app.use(ejsLayouts);
 app.use('/pokemon', require('./routes/pokemon'));
-
-// GET / - main index of site
-// app.get('/', (req, res) => {
-//   let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
-//   axios.get(pokemonUrl).then(apiResponse => {
-//     let pokemon = apiResponse.data.results;
-//     res.render('index', { pokemon: pokemon.slice(0, 151) });
-//   })
-// });
 
 // GET / - MAIN MAP
 app.get('/', (req, res) => {
@@ -35,10 +27,19 @@ app.get('/', (req, res) => {
 })
 
 // GET /ROUTE - VIEW ROUTE
-// app.get('/:id', (req, res) => {
-//   let name = req.params.id
-//   res.render('route.ejs')
-// })
+app.get('/:id', (req, res) => {
+  db.allplace.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((oneplace) => {
+    res.render('route.ejs', {oneplace: oneplace})
+  })
+  .catch(err => {
+    log(err)
+  })
+})
 
 // TEST ROUTE
 // app.get('/', (req, res) => {
@@ -49,7 +50,26 @@ app.get('/', (req, res) => {
 //   })
 // })
 
-
 app.listen(port, () => {
   console.log('...listening on', port );
 });
+
+
+
+// SCRAP CODE
+
+// TEST LOGS FOR ACCESSING ALLPLACES INFO
+// allplaces.forEach((e) => {
+//   log(e.dataValues.id)
+// })
+// log(allplaces[0].dataValues.id)
+
+    
+// GET / - show main pokedex
+// app.get('/', (req, res) => {
+//   let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
+//   axios.get(pokemonUrl).then(apiResponse => {
+//     let pokemon = apiResponse.data.results;
+//     res.render('index', { pokemon: pokemon.slice(0, 151) });
+//   })
+// });
