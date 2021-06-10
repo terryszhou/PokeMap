f1e24964d482064b86b19eb1d672ca2872ebfbdf
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-10 17:01:08 -0500

    Finished planning routes, added two new places.

--- db-test.js
+++ db-test.js
@@ -1,15 +1,27 @@
 const db = require('./models');
 
-db.pokemon.create({
-    name: 'pikachu'
-  }).then(poke => {
-    console.log('Created: ', poke.name)
-  })
+// db.pokemon.create({
+//     name: 'pikachu'
+//   }).then(poke => {
+//     console.log('Created: ', poke.name)
+//   })
 
-db.pokemon.findOne({
-  where: {
-    name: 'pikachu'
-  }
-}).then(poke => {
-  console.log('Found: ', poke.name)
+// db.pokemon.findOne({
+//   where: {
+//     name: 'pikachu'
+//   }
+// }).then(poke => {
+//   console.log('Found: ', poke.name)
+// })
+
+db.allplace.create({
+  name: 'seafoam islands'
+}).then(place => {
+  console.log(`Created: ${place.name}`)
+})
+
+db.allplace.create({
+  name: 'special event'
+}).then(place => {
+  console.log(`Created: ${place.name}`)
 })
